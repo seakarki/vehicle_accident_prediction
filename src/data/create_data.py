613429b2e411658057be754e7ac4297b59b8aaa7
Sammy <friendly19234@gmail.com>
2021-02-22 20:32:40 -0700

    changed dataset

--- src/data/create_data.py
+++ src/data/create_data.py
@@ -15,6 +15,7 @@ logging.info('I told you so')
 
 faker = Faker()
 
+## Create the driver profile
 driver_name = [faker.name() for i in range(1000)]
 driver_age = [random.randint(18,60) for i in range(1000)]
 driver_alcholic = [random.choice(['yes','no']) for i in range(1000)]
@@ -36,8 +37,28 @@ driver_profile = pd.DataFrame(list(zip(driver_name,
                                driver_license_age)),columns=columns)
 print(driver_profile.head())
 
+## Create the vehicle profile
+
+driver_name = [faker.name() for i in range(1000)]
+driver_age = [random.randint(18,60) for i in range(1000)]
+driver_alcholic = [random.choice(['yes','no']) for i in range(1000)]
+driver_has_kids =[random.choice(['yes','no']) for i in range(1000)]
+driver_marital_status =[random.choice(['S','M']) for i in range(1000)]
+driver_ownership =[random.choice(['yes','no']) for i in range(1000)]
+driver_license_age= [random.randint(2,25) for i  in range(1000)]
+
+columns = ['driver_name','driver_age','is_driver_alcholic','is_driver_has_kids',
+           'driver_marital_status','driver_ownership','driver_license_age']
 
 
+driver_profile = pd.DataFrame(list(zip(driver_name,
+                               driver_age,
+                               driver_alcholic,
+                               driver_has_kids,
+                               driver_marital_status,
+                               driver_ownership,
+                               driver_license_age)),columns=columns)
+print(driver_profile.head())
 
 
 
