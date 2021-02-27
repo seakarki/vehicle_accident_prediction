fbced965337163d8cfc2ef950eb89f4fe6b67136
Sammy <friendly19234@gmail.com>
2021-02-27 15:41:03 -0700

    creation of fake datasets for accident

--- src/data/create_data.py
+++ src/data/create_data.py
@@ -2,16 +2,12 @@
 from faker import Faker
 import pandas as pd
 import random
+
+
+
 import logging
-
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-logging.debug('This message should appear on the console')
-logging.info('So should this')
-logging.warning('And this, too')
-
-
-logging.warning('Watch out!')
-logging.info('I told you so')
+#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+#logging.debug('This is a log message.')
 
 faker = Faker()
 
@@ -27,7 +23,6 @@ driver_license_age= [random.randint(2,25) for i  in range(1000)]
 columns = ['driver_name','driver_age','is_driver_alcholic','is_driver_has_kids',
            'driver_marital_status','driver_ownership','driver_license_age']
 
-
 driver_profile = pd.DataFrame(list(zip(driver_name,
                                driver_age,
                                driver_alcholic,
@@ -35,31 +30,47 @@ driver_profile = pd.DataFrame(list(zip(driver_name,
                                driver_marital_status,
                                driver_ownership,
                                driver_license_age)),columns=columns)
-print(driver_profile.head())
+print("The datasets for driver: \n", driver_profile.head())
+
 
 ## Create the vehicle profile
 
-driver_name = [faker.name() for i in range(1000)]
-driver_age = [random.randint(18,60) for i in range(1000)]
-driver_alcholic = [random.choice(['yes','no']) for i in range(1000)]
-driver_has_kids =[random.choice(['yes','no']) for i in range(1000)]
-driver_marital_status =[random.choice(['S','M']) for i in range(1000)]
-driver_ownership =[random.choice(['yes','no']) for i in range(1000)]
-driver_license_age= [random.randint(2,25) for i  in range(1000)]
+vehicle_year = [random.randint(2000,2020) for i in range(1000)]
+vehicle_age = [random.randint(1,25) for i in range(1000)]
+vehicle_mileage = [random.randint(5000,300000) for i in range(1000)]
+vehicle_last_maintaince = [random.randint(2015,2020) for i in range(1000)]
 
-columns = ['driver_name','driver_age','is_driver_alcholic','is_driver_has_kids',
-           'driver_marital_status','driver_ownership','driver_license_age']
+columns = ['vehicle_year','vehicle_age','vehicle_mileage','vehicle_last_maintaince']
 
-
-driver_profile = pd.DataFrame(list(zip(driver_name,
-                               driver_age,
-                               driver_alcholic,
-                               driver_has_kids,
-                               driver_marital_status,
-                               driver_ownership,
-                               driver_license_age)),columns=columns)
-print(driver_profile.head())
+vehicle_profile = pd.DataFrame(list(zip(vehicle_year,
+                               vehicle_age,
+                               vehicle_mileage,
+                               vehicle_last_maintaince)),columns=columns)
+print("The datasets for vehicle: \n", vehicle_profile.head())
 
 
+## Create the road profile
+
+road_condition = [random.choice(['pitch','gravel','off_road']) for i in range(1000)]
+season = [random.choice(['rainy','winter','summer','autumn']) for i in range(1000)]
+road_terrain = [random.choice(['hilly','mountain','terai']) for i in range(1000)]
+day = [random.randint(1,29) for i in range(1000)]
+
+month= [random.randint(1,12) for i in range(1000)]
+
+
+columns = ['road_condition','season','road_terrain','day','month']
+road_profile = pd.DataFrame(list(zip(road_condition,season,road_terrain,day,month)),columns=columns)
+
+print("The datasets for road: \n", road_profile.head())
+
+### Combine three datasets  together
+
+driver_vehicle_road_df = pd.concat([driver_profile,vehicle_profile,road_profile],axis = 1)
+print("The final datasets:\n",driver_vehicle_road_df.head())
+
+## save the final datasets as csv
+driver_vehicle_road_df.to_csv(
+    "/Users/ocean/PycharmProjects/vehicle_accident_prediction/data/driver_vehicle_road_df.csv",index=False)
 
 
